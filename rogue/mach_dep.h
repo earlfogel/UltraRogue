5a6d1874b02ca4d9ad2322fa49ffc59ef7d4e836
Earl Fogel <earl.fogel@usask.ca>
2019-10-29 09:55:39 -0600

    support <SHIFT> and <CTRL> arrow keys on linux; switch from *rand48 to *random functions

--- rogue/mach_dep.h
+++ rogue/mach_dep.h
@@ -8,20 +8,14 @@
 
 #ifdef	BSD4
 #include <stdlib.h>
+#endif
 
 #ifdef _WIN32
-#define	srand48	srand
-#define	lrand48	rand
-#else
-#define	strchr	index
-#define srand48	srandom
-#define lrand48	random
-#endif
+#define random rand
+#define srandom srand
 #endif
 
 #ifdef	USGV3
-#define	srand48	srand
-#define	lrand48	rand
 #define	_doprnt	_dprnt
 #define	setegid	setgid
 #define	seteuid	setuid
