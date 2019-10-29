ca7ad70148f42d940b9b64e6a15b7e85854bbedc
Earl Fogel <earl.fogel@usask.ca>
2019-10-29 10:01:28 -0600

    update version string

--- rogue/vers.c
+++ rogue/vers.c
@@ -1,5 +1,5 @@
 /*
  * version number  
  */
-char version[] = "UltraRogue 1.08 August 2019";
-char *release = "1.08 (August 2019)";
+char version[] = "UltraRogue 1.08 October 2019";
+char *release = "1.08 (October 2019)";
