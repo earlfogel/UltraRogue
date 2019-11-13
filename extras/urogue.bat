34a42bf85c73710480a8d7eefcfd82af1ef5cf20
Earl Fogel <earl.fogel@usask.ca>
2019-11-12 19:32:40 -0600

    include example showing how to set options

--- extras/urogue.bat
+++ extras/urogue.bat
@@ -1,11 +1,15 @@
 @echo off
-REM Run UltraRogue
-REM If we have a saved game, resume it, otherwise start a new one.
+rem Run UltraRogue
+
+rem Set Options:
+set SROGUEOPTS=cutcorners,difficulty=normal
+
+rem If we have a saved game, resume it, otherwise start a new one.
 if exist %APPDATA%\urogue\rogue.save (
+	copy /y %APPDATA%\urogue\rogue.save %APPDATA%\urogue\rogue.save.bak >NUL
 	urogue.exe %APPDATA%\urogue\rogue.save
 ) else (
-	REM add game options to the following line, e.g.: urogue.exe -easy
-	urogue.exe
+	urogue.exe %*
 )
 echo.
 pause
--- extras/urogue.sh
+++ extras/urogue.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
+# set various options
+#export SROGUEOPTS=cutcorners,difficulty=normal
+
+# If we have a saved game, resume it, otherwise start a new one.
 if [ -e ~/rogue.save ] ; then
     urogue ~/rogue.save
 else
     # add game options to the following line, e.g. "urogue -easy"
-    urogue
+    urogue $@
 fi
